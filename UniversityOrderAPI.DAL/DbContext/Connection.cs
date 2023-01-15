f768dc4d88de2ebd4d07e8e1e22b143b758e13b5
nicklson <nicklson1@yandex.ru>
2023-01-15 16:24:44 +0700

    ConnectionHelper

---
+++ UniversityOrderAPI.DAL/DbContext/Connection.cs
@@ -0,0 +1,6 @@
+namespace UniversityOrderAPI.DAL;
+
+public static class ConnectionHelper
+{
+    public static string ConnectionString = "Host=5.188.119.237;Port=5432;Database=UniversityOrder;Username=universityOrderApi;Password=jSujyE^^4QE%Vp8uc$QR4a";
+}
--- UniversityOrderAPI.DAL/Program.cs
+++ UniversityOrderAPI.DAL/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using UniversityOrderAPI.DAL;
+
+Console.WriteLine("Hello, World!");
+
+using var db = new UniversityOrderAPIDbContext();
+
+Console.WriteLine();
