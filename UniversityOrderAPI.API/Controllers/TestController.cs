24a7dbb56299df292028b65752580d4c70e3a099
Nick <nicklson1@yandex.ru>
2023-01-15 12:42:32 +0700

    someTestClass and controller

---
+++ UniversityOrderAPI.API/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniversityOrderAPI.DAL;
+
+namespace UniversityOrderAPI.Controllers;
+
+public class SomeTestClass
+{
+    public int A { get; set; }
+    public int D { get; set; }
+}
+
+[ApiController]
+[Route("[controller]")]
+public class TestController:BaseApiController
+{
+    public TestController(UniversityOrderAPIDbContext db) : base(db)
+    {
+    }
+
+
+    [HttpGet("some")]
+    public SomeTestClass GetSomeTestClass()
+    {
+        return new SomeTestClass();
+    }
+}
