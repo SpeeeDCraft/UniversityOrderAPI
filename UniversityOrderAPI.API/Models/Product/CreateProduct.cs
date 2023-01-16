b58d2cd5c45adcdeb982dda7907cc29b09b42b3d
Dmitry <52341158+SpeeeDCraft@users.noreply.github.com>
2023-01-16 18:10:40 +0700

    product-behavior

---
+++ UniversityOrderAPI.API/Models/Product/CreateProduct.cs
@@ -0,0 +1,23 @@
+﻿namespace UniversityOrderAPI.Models.Product;
+
+public class CreateProductRequest
+{
+    public int CategoryId { get; set; }
+
+    public int ManufacturerId { get; set; }
+
+    public string Name { get; set; }
+
+    public int Cost { get; set; }
+
+    public string Description { get; set; }
+    
+    public DAL.Models.Category Category { get; set; }
+    public DAL.Models.Manufacturer Manufacturer { get; set; }
+    
+}
+
+public class CreateProductResponse : ISingleResult<ProductAPIDTO>
+{
+    
+}
---
+++ UniversityOrderAPI.API/Models/Product/ProductAPIDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace UniversityOrderAPI.Models.Product;
+
+public class ProductAPIDTO
+{
+    public int Id { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public int ManufacturerId { get; set; }
+
+    public string Name { get; set; }
+
+    public int Cost { get; set; }
+
+    public string Description { get; set; }
+}
